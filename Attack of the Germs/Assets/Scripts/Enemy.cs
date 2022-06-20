71ef77c1da924b379c703d779ceb84dcefd787aa
Amanda797 <agrose071479@live.com>
2022-06-20 14:34:38 -0400

    explosion particle effect created

--- Attack of the Germs/Assets/Scripts/Enemy.cs
+++ Attack of the Germs/Assets/Scripts/Enemy.cs
@@ -11,6 +11,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] float maxTimeBetweenShots = 3f;
     [SerializeField] GameObject projectile;
     [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOFExplosion = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -58,7 +60,15 @@ public class Enemy : MonoBehaviour
         damageDealer.Hit();
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOFExplosion);
     }
 }
